ced01bd544077888f39fa9e57af47919b85ca013
alishkdk <kdkalish4@gmail.com>
2025-12-01 13:26:21 +0545

    create user add

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 const PORT=8000;
+const fs=require('fs')
 const users=require('./MOCK_DATA.json');
 
 
@@ -9,6 +10,8 @@ app.get("/api/users",(req,res)=>{
 return res.json(users);
 });
 
+//middleware -plugin
+app.use(express.urlencoded({extended:false}));
 
 app
 .route("/api/users/:id")
@@ -16,6 +19,15 @@ app
     const id=Number(req.params.id);
     const user=users.find((user)=>user.id===id);
     return res.json(user);
+});
+
+app.post("/api/users",(req,res)=>{
+    const body=req.body;
+    users.push({...body, id: users.length + 1});
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
+     
+        return res.json({status:"id:users"});
+    });
 });
 
 app.listen(PORT,() => console.log('server start at port:${PORT}'));
